72b44e058bf5ab8c408179bebcef81e6390acda9
Gustavo-de-Oliveira <gustavo_de_oliveira@usp.br>
2018-09-02 00:35:01 -0300

    Versão 0.02

--- knn.c
+++ knn.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct atributos{
+	float sepeal_length;
+	float sepal_width;
+	float petal_length;
+	float petal_width;
+}atributos;
+
 int main(int argc, char const *argv[]){
-	
+	char nome[9];
+	FILE *treino = NULL, *exemplo = NULL;
+	int k = 0;
+
+	scanf("%s", nome);
+	treino = fopen(nome, "r");
+
+	scanf("%s", nome);
+	exemplo = fopen(nome, "r");
+
+	scanf("%d", &k);
+
 	return 0;
 }
